96f4ac0d1ca6fd31b616178d050711bd8a3f8c5d
DANISH468 <122468304+DANISH468@users.noreply.github.com>
2023-01-25 18:44:52 +0400

    Update dataTable.js

--- lwc/datatable/dataTable.js
+++ lwc/datatable/dataTable.js
@@ -1,37 +1,46 @@
 import { LightningElement,api } from 'lwc';
 import executeApex from '@salesforce/apex/DataTableCtrl.executeApex';
 
+
 export default class DataTable extends LightningElement {
 
     
     @api records;
     @api tableColumns;
-
+    updatedColumns;
+    @api updatedRecords;
+    
     connectedCallback(){
-
-        this.fetchModifiedColumns();       
+   
+        this.fetchModifiedColumns();
+        this.updatedRecords = JSON.parse(JSON.stringify(this.records));  
     }
-
-
 
     fetchModifiedColumns(){
 
-     
-        let columnsJson = JSON.stringify(this.tableColumns);
-        let paramsObj = 
-            {'columns' : columnsJson }
     
-        executeApex({action : 'modifyColumns' , parameters : paramsObj}).then(response => {
-            this.updatedColumns = response;
-        
+        let columnsJson = JSON.stringify(this.tableColumns);
+        if(columnsJson == undefined || this.records[0]?.Id == undefined) return;
+        let paramsObj = 
+            {
+                'columns' : columnsJson,
+                'recordId' : this.records[0]?.Id
+            }
+         
+        executeApex(
+            {action : 'modifyColumns' , parameters : paramsObj}
+            ).then(response => {
+                this.updatedColumns = response;   
+             
         }).catch(error => {
 
         });
     }
 
-    renderedCallback(){
-      
+    handleCustomType(event){
+        
+        let searchedRecord = this.updatedRecords.find(record => record.Id == event.detail.selectedId); 
+        searchedRecord[event.detail.fieldApiName] = event.detail.value;
+        this.updatedRecords = [...this.updatedRecords];
     }
-
-
 }
